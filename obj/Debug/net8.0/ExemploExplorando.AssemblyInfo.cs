5092bec38bfd2889c8a3a2b3234e68f78b4ba1e8
ceduardooliveira <c-eduardo94@hotmail.com>
2024-08-30 17:08:48 -0300

    ✨ feat: add course class

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02161b5452d4829a68932ba27b3a54566512eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b694709181f6b1b62274ec6dbdc4eea377ef06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
