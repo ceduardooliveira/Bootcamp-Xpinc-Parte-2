8af5a6ebc98612d20eaf3f3b1b226787fe7f0777
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-04 13:22:17 -0300

    ✨ feat: add code location

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbc3f0700669b97ca973fe31cad59f48d9ff661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c249a1e0f0394fa941e21ba44d4c9317ebee9cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
