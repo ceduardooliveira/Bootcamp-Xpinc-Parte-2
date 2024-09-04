e61bebb5e0777a4846e613899e18fe5e18b1af0f
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-04 10:51:53 -0300

    ✨ feat: add string interpolation

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2436a1b9604d0f1ee5fed745b9729a51aef83be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44783e806c4a44715cafd5748b0ab65d893878a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
