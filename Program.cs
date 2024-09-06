5013e7d652507d5dcf7b562bf4033f2d74e8da42
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-06 20:50:00 -0300

    ✨ feat: add tuples in practice

--- Program.cs
+++ Program.cs
@@ -2,13 +2,47 @@
 using System.Globalization;
 
 
-Dictionary<string, string> estados = new Dictionary<string, string>();
+(int, string, string, decimal) tupla = (1, "Eduardo", "Oliveira", 1.69M);
 
-estados.Add("SP", "São Paulo");
-estados.Add("BA", "Bahia");
-estados.Add("MG", "Minas Gerais");
+Console.WriteLine($"ID: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
 
-Console.WriteLine(estados["MG"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dictionary<string, string> estados = new Dictionary<string, string>();
+
+// estados.Add("SP", "São Paulo");
+// estados.Add("BA", "Bahia");
+// estados.Add("MG", "Minas Gerais");
+
+// Console.WriteLine(estados["MG"]);
+
+
+
 
 // foreach(var item in estados)
 // {
