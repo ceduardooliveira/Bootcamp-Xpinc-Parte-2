e80df5a715fc9c67b8798ce3fe1db59bd2fc32f0
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-06 19:29:48 -0300

    ✨ feat: add stack

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836bc39424a73b632fa53aae49023f33823e186d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2930ad1dc8172d9236173d41fcfca11b0c7eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
