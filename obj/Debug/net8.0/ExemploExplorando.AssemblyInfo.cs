dfc70c842245fe8b5244228e88962023c1122572
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-09 20:48:24 -0300

    ✨ feat: add write a json file

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af800da7b5ddeac7e80e74539ccb848e661458f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c10b3360409e422e62232da8fe236d6345617b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
