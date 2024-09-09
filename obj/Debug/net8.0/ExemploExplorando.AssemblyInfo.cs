af800da7b5ddeac7e80e74539ccb848e661458f6
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-09 20:13:24 -0300

    ✨ feat: add nuget installation

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0146dfd967c0e33d8947447a18d6731580650d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c4dfacaee193eabb344d7d10843e9b205af484")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
