22349ea4702460c38b86640ba2cbdf5e692aa78c
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-09 22:10:47 -0300

    ✨ feat: add attribute

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295e4c6b2320edd1543be2d6172007903336e3f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f426b27664a7ae3ef37ee244a3507fa041a3f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
